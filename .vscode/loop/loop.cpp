e00c7c560d7fecfa15dce776e8913103948fdd00
Sudhanshu Singh <sudhanshusingh15may@gmail.com>
2025-10-12 23:21:11 +0530

    this is my first loop on github

---
+++ .vscode/loop/loop.cpp
@@ -0,0 +1,118 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+// int i,n,prev,current,last;
+// cin>>n;
+// prev=0;
+// last=1;
+// for ( i = 0; i < n; i++)
+// {
+ 
+//     current=prev+last;
+//     prev=last;
+//     last=current;
+// cout<<prev <<"  ";
+   
+
+// }
+
+// return 0;
+
+
+
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int row,col,num,count;
+//    count=1;
+//     for ( row=1; row<=5; row=row+1)
+//     {
+
+//         for ( col = 1; col <= 5; col=col+1)
+
+//         {
+//            cout<<count <<" ";
+//            count=count+1;
+
+//         }
+//          cout<<endl;
+
+//     }
+//     return 0;
+
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//    int row,col ;
+
+//    for ( row = 1; row <=5; row=row+1)
+//    {
+//       for (col=5; col>=5-row+1; col=col-1)
+//       {
+
+//         cout<<col<<" ";
+
+//       }
+//       cout<<endl;
+//    }
+
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//    cout << "h";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//    int row, col;
+//    char name;
+//    for (row = 1; row<=5; row++){
+//       for (col = 1; col<=5-row; col++){
+      
+//          cout << "  ";
+
+//       }
+   
+//          for (col=row; col>=1; col--)
+//          {
+             
+//             cout <<col<<" " ;
+//          }
+      
+//          cout << endl;
+      
+//    }
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//    int row,col,star;
+//    for ( row = 1; row <= 5; row++)
+//    {
+//       /* code */
+//       for ( col = 1; col <=5-row; col++)
+//       {
+//          cout<<" ";
+//       }
+//       for (star=1; star <=row ; star++)
+//       {
+//         cout<<"*";
+//       }
+//       cout<<endl;
+      
+//    }
+   
+// }
