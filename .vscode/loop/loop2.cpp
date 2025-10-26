a38b73b5a5a4757dc9e78117d7ec55c0c6425150
Sudhanshu Singh <sudhanshusingh15may@gmail.com>
2025-10-26 11:55:33 +0530

    change kro

---
+++ .vscode/loop/loop2.cpp
@@ -0,0 +1,160 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int row,col,n,i,j;
+//     cout <<"enter n";
+//     cin>>n;
+//     for (row =1; row<=n; row++)
+//     {
+//        for ( col=1; col<n-row ; col++)
+//        {
+//         cout<<" ";
+//        }
+//        for ( i = 1; i<=row ; i++)
+//        {
+//         cout<<i;
+//        }
+//        for ( j=row-1; j>=1; j--)
+//        {
+//        cout<<j;
+//        }
+//        cout<<endl;
+       
+  
+//     }
+    
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   int row,col,n,i,j;
+//     cout <<"enter n";
+//     cin>>n;
+//     for (row =n; row>=1; row=row-1)
+//     {
+//        for ( col=1; col<=n-row; col++)
+//        {
+//         cout<<"  ";
+//        }
+//        for ( i =1; i<=2*row-1 ; i++)
+//        {
+//         cout<<"* ";
+
+//        }
+
+//        cout<<endl;
+
+//     }
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col,n,star;
+//     cout<<"enter n here";
+//     cin>>n;
+//     for(row=n; row>=1; row=row-1){
+//     for(col=1; col<=row;col++)
+//      cout<<"* ";
+    
+//     for(col=1;col<=2*n-2*row; col++)
+//        cout<<"  ";
+    
+//     for(star=1;star<=row; star++)
+//        cout<<"* ";
+    
+//     cout<<endl;
+//     }
+
+//     for(row=1; row<=n; row=row+1){
+//     for(col=1; col<=row;col++)
+//      cout<<"* ";
+    
+//     for(col=1;col<=2*n-2*row; col++)
+//        cout<<"  ";
+    
+//     for(star=1;star<=row; star++)
+//        cout<<"* ";
+    
+//     cout<<endl;
+//     }
+
+// }
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row ,col,n;
+//    cout<<"enter n here";
+//    cin>>n;
+
+
+
+//    for(row=1;row<=n;row++){
+
+//       for(col=1;col<=row;col++)
+//       cout<<"* ";
+//       for(col=1;col<=2*n-2*row;  col++)
+//       cout<<"  ";
+//       for(col=1; col<=row; col++)
+//       cout<<"* ";
+//        cout<<endl;
+        
+
+//    }   
+
+
+
+//    for(row=n-1;row>=1;row--){
+
+//       for(col=1;col<=row;col++)
+//       cout<<"* ";
+//       for(col=1;col<=2*n-2*row;  col++)
+//       cout<<"  ";
+//       for(col=1; col<=row; col++)
+//       cout<<"* ";
+//        cout<<endl;
+
+// }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int n,i;
+//     cout<<"enter here";
+//     cin>> n;
+//      i=1;  // initialize
+//     while (i<=n){  //=> check
+   
+//     if (n%i==0){
+    
+//        cout<<i<<endl;
+     
+//     }
+    
+//     i++; // update
+// }
+// }
+
+
+#include <iostream>
+using namespace std;
+int main (){
+    int  sum=0;
+    int i=1;
+   do
+   {
+       sum=sum+i;
+       cout<<sum<<endl;
+       i++;
+   } while (i<=10);
+   
+    
+}
+
---
+++ .vscode/loop/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+ for(col=1;col<=2*n-2*row; col++){
+   //     cout<<" ";
+   //  }
+   //  for(star=row;star>=row; star--){
+   //     cout<<"* ";
+   //  }
